85427f99a9a72bc68e62dd792f3c78fe4d314908
rituraj <riturajroj1234@gmail.com>
2022-09-28 22:35:41 +0530

    index.js file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+// ReactDOM.render(<App/>, document.getElementById(root));
+// registerServiceWorker();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
